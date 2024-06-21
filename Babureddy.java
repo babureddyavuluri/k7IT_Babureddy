e1f41c7bbbf77083ff5893e70e0c875eb801824d
babureddyavuluri <babureddyavuluri@gmail.com>
2024-06-21 17:58:56 +0530

    Add files via upload

---
+++ Babureddy.java
@@ -0,0 +1,19 @@
+class Babureddy{
+int age;
+string name;
+double ht;
+double wt;
+
+public static void main(String args[]){
+Babureddy b = new Babureddy();
+b.age = 30;
+b.name = "Babu"
+b.ht = 5.6;
+b.wt = 68.54;
+
+System.out.println(b.age);
+System.out.println(b.name);
+System.out.println(b.ht);
+System.out.println(b.wt);
+}
+}
