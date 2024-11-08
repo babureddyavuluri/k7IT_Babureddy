7219007a8bf5b8a489586d88bf518b2a68ab6e37
babureddyavuluri <babureddyavuluri@gmail.com>
2024-11-08 12:57:06 +0530

    Add files via upload

---
+++ Reddy.java
@@ -0,0 +1,15 @@
+class Reddy extends Babu1{
+//int i=20;
+
+void m2(){
+System.out.println("Reddy-m2");
+
+}
+public static void main(String args[]){
+Reddy r=new Reddy();
+System.out.println(r.i);
+r.m1();
+r.m2();
+}
+
+}
